5b2ed988f6521caf9b99bd292950443fdfaf375c
imransajeb <imran.ih232@gmail.com>
2021-01-12 03:33:55 +0600

    commit all

---
+++ shared.js
@@ -0,0 +1,50 @@
+let backdrop = document.querySelector('.backdrop');
+let modal = document.querySelector('.modal');
+let modalNoButton = document.querySelector(".modal__action--negative");
+let selectPlanButton = document.querySelectorAll('.plan button');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
+
+// console.dir(background.style['backround-image']);
+
+// console.dir(backdrop);
+for ( let i = 0; i<selectPlanButton.length; i++){
+    selectPlanButton[i].addEventListener('click', function(){
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = "open"; // This will actully overwriten the complete class list
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+    });
+}
+
+
+
+backdrop.addEventListener("click", function() {
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+if(modalNoButton){
+    modalNoButton.addEventListener("click", closeModal);
+}
+
+function closeModal() {
+    // backdrop.style.display = "none";
+    // modal.style.display = "none";
+    if(modal){
+        modal.classList.remove('open');
+    }
+
+    backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function(){
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+
+});
+
